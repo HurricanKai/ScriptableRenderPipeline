9fc14c0812864424f1973c9a721d636a8c4e9f14
Thomas <chman.a11w@gmail.com>
2018-12-12 16:20:52 +0100

    Fixed pre-exposure in atmospheric scattering

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/AtmosphericScattering/AtmosphericScattering.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/AtmosphericScattering/AtmosphericScattering.hlsl
@@ -23,7 +23,7 @@ float3 GetFogColor(float3 V, float fragDist)
         // Based on Uncharted 4 "Mip Sky Fog" trick: http://advances.realtimerendering.com/other/2016/naughty_dog/NaughtyDog_TechArt_Final.pdf
         float mipLevel = (1.0 - _MipFogMaxMip * saturate((fragDist - _MipFogNear) / (_MipFogFar - _MipFogNear))) * _SkyTextureMipCount;
         // For the atmosphéric scattering, we use the GGX convoluted version of the cubemap. That matches the of the idnex 0
-        return SampleSkyTexture(-V, mipLevel, 0).rgb * GetCurrentExposureMultiplier();
+        return SampleSkyTexture(-V, mipLevel, 0).rgb;
     }
     else // Should not be possible.
         return  float3(0.0, 0.0, 0.0);
@@ -111,7 +111,7 @@ float4 EvaluateAtmosphericScattering(PositionInputs posInput, float3 V)
         }
     }
 
-    return float4(fogColor, fogFactor);
+    return float4(fogColor * GetCurrentExposureMultiplier(), fogFactor);
 }
 
 #endif
